8ffced185f20628c95d3a8158bfa52acfe92270b
MrWhok <151937580+MrWhok@users.noreply.github.com>
2024-07-03 13:28:29 +0700

    Update Lemur XOR.py

--- general/XOR/Lemur XOR.py
+++ general/XOR/Lemur XOR.py
@@ -1,1 +1,8 @@
+from PIL import ImageChops,Image
+
+im1 = Image.open("lemur.png")
+im2 = Image.open("flag.png")
+
+im3 = ImageChops.add(ImageChops.subtract(im1, im2), ImageChops.subtract(im2, im1))
+im3.show()
 
