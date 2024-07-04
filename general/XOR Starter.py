758f799a74ef93cfe2a56626f7e6e16c2f9821ec
MrWhok <151937580+MrWhok@users.noreply.github.com>
2024-07-05 06:40:42 +0700

    Create XOR Starter.py

---
+++ general/XOR Starter.py
@@ -0,0 +1,8 @@
+from pwn import xor
+
+flag=""
+for i in 'label':
+    temp=xor(i.encode(),13) #xor function expects bytes so we need to convert it first 
+    temp=temp.decode()
+    flag+=temp
+print(flag)
